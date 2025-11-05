3017c9f828b199ad1265b0430c9b2a0dc657a37f
jeremytheva <jeremytheva@gmail.com>
2025-11-06 09:00:51 +1100

    Update index.ts

--- app/components/ui/index.ts
+++ app/components/ui/index.ts
@@ -1,15 +1,7 @@
- (cd "$(git rev-parse --show-toplevel)" && git apply --3way <<'EOF' 
-diff --git a/app/components/ui/index.ts b/app/components/ui/index.ts
-deleted file mode 100644
-index 2e018bd4e964130769ab394b50a25d185e5adf81..0000000000000000000000000000000000000000
---- a/app/components/ui/index.ts
-+++ /dev/null
-@@ -1,5 +0,0 @@
--export { Button } from "./button";
--export { Card } from "./card";
--export { Input } from "./input";
--export { Modal } from "./modal";
--export { NavTabs } from "./nav-tabs";
+export { Button } from "./button";
+export { Card } from "./card";
+export { Input } from "./input";
+export { Modal } from "./modal";
+export { NavTabs } from "./nav-tabs";
  
-EOF
-)
+
